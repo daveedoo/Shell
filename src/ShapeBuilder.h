1a5773638d50385418dc61984af46581e2e4db99
Dawid Maksymowski <damaks48@gmail.com>
2023-05-22 19:39:10 +0200

    Separate method for shelling + logging directive

--- src/ShapeBuilder.h
+++ src/ShapeBuilder.h
@@ -5,5 +5,6 @@ class ShapeBuilder
 {
 public:
 	static TopoDS_Shape Bottle(const Standard_Real myWidth, const Standard_Real myHeight, const Standard_Real myThickness);
-	static TopoDS_Shape Shell(bool doFillet, bool showRawShape = false);
+	static TopoDS_Shape TheShape(bool doFillet, bool showRawShape = false);
+	static TopoDS_Shape Shell(const TopoDS_Shape& originalShape, Standard_Real thickness = -0.5);
 };
